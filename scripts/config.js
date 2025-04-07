bf53ff90af6e3c22240df22b529cb30151ce24ed
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2025-04-07 18:04:16 +0200

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,3 +1,2 @@
 window.GOOGLE_SHEETS_DATA = "https://script.google.com/macros/s/AKfycby3KKSts4g0TfbhztXv75F7DIwnYi42g0ddC7J2B6KXL6_gXe3HPdiP33Z_xCFRnybDIg/exec"
 window.USE_LIVE_DATA = true; // skift til false for at bruge lokal data
-
